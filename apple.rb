a093d0f203e6edacb5c3fe14e2325ccb5f6a3272
EC2 Default User <ec2-user@ip-172-31-37-111.ap-northeast-1.compute.internal>
2025-06-30 04:18:51 +0000

    [finish] 2-5

---
+++ apple.rb
@@ -0,0 +1,10 @@
+#apple = "Nagano"
+apple = "Yamanashi"  # この行を変更、変更前一行目#内
+
+if apple == "Aomori"
+  puts "このリンゴは青森県産です。"
+elsif apple == "Nagano"
+  puts "このリンゴは青森県産ではなく、長野県産です。"
+else
+  puts "このリンゴは青森県産でも長野県産でもありません。"
+end
